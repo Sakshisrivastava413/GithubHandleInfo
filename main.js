a13a01120f33b68379c3aa887dd8302f74a7a05e
Sakshi <sakshisrivastava413@gmail.com>
2018-04-22 20:53:36 +0530

    user basic profile support added

--- main.js
+++ main.js
@@ -19,21 +19,20 @@ new Vue({
   el: "#app",
   data: {
     input: '',
-
+    userInfo: {
+    },
     repos: []
   },
   methods: {
     init() {
-      // this.$http.get('https://api.github.com/users/' + this.input)
-      //   .then(function(response1) {
-      //     console.log(response1.data);
-      //   });
-// res.data kya aara hai? array  of obj
+      this.$http.get('https://api.github.com/users/' + this.input)
+        .then(response => {
+          console.log(response.data);
+          this.userInfo=response.data
+        });
       this.$http.get('https://api.github.com/users/' + this.input + "/repos")
         .then(res => {
           console.log(res.data)
-          // meri jaan simply repos ko res.data ke equal karegi to kya hoyega?
-          // acha hn right
           this.repos=res.data
         });
     }
